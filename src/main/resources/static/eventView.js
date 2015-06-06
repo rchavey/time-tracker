99ac3852ee696aecd6f86f75abdc2271a4aabc65
rchavey <rchavey@gmail.com>
2015-06-05 23:14:04 -0500

    js: separated function code for eventView from graphical button html: added in new js file html: coded campaignView.html

--- src/main/resources/static/eventView.js
+++ src/main/resources/static/eventView.js
@@ -1,22 +1,5 @@
 $(document).ready(function(){
 
-    //animation on buttons
-    $(".button").mousedown(function() {
-        $(this).css({
-            "background-color": "rgba(5, 5, 60, .75)",
-            "box-shadow": "2px 2px 2px #333 inset",
-            "border": "1px inset #FFF",
-            "color": "#BBB"
-        });
-    }).mouseup(function() {
-        $(this).css({
-            "background-color": "rgba(10, 10, 100, .75)",
-            "border": "1px outset #BBB",
-            "box-shadow": "2px 2px 2px #333",
-            "color": "#FFF"
-        });
-    });
-
     // functionality of adding time:
     $(".timeButton").click(function () {
 
@@ -30,7 +13,7 @@ $(document).ready(function(){
     });
 
     $("#saveButton").click(function () {
-        var saved = (timeToSeconds();
+        var saved = (timeToSeconds());
         console.log(saved);
     });
 
