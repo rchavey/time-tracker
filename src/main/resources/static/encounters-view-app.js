6c4bc3105c3276ccba49859ca4c0b15fbbba9fc5
rchavey <rchavey@gmail.com>
2015-07-09 22:00:09 -0500

    Added wittig's monster tracking

---
+++ src/main/resources/static/encounters-view-app.js
@@ -0,0 +1,27 @@
+(function() {
+
+    var app = angular.module('encounters', []);
+
+    app.controller('EncounterViewController', ['$http', function($http) {
+        var self = this;
+
+        this.encounters = [];
+
+        $http.get('/time-tracker/controller/encounters').success(function(data){
+            self.encounters = data;
+        });
+
+        this.selectedEncounter = {};
+
+        this.encounterDetail = {};
+
+        this.viewEncounter = function() {
+
+            $http.get('/time-tracker/controller/encounter/'+self.selectedEncounter.id).success(function(data){
+                self.encounterDetail = data;
+            });
+        }
+
+    }]);
+
+})();
