39cec92ec86d23e1b6f2621a3298c83be2c5fc17
rchavey <rchavey@gmail.com>
2015-08-06 18:39:56 -0500

    calculate monster hits

--- src/main/resources/static/encounters-view-app.js
+++ src/main/resources/static/encounters-view-app.js
@@ -2,7 +2,7 @@
 
     var app = angular.module('encounters', []);
 
-    app.controller('EncounterViewController', ['$http', function($http) {
+    app.controller('EncounterViewController', ['$http', '$timeout', function($http, $timeout) {
         var self = this;
 
         this.encounters = [];
@@ -29,6 +29,52 @@
                 self.elapsedTime.days = time.days;
                 self.elapsedTime.hours = time.hours;
                 console.log(self.elapsedTime);
+
+                $timeout(function() {
+                    var monsterTypes = $(.monster-type-container);
+                    //cycle though each monster type
+                    for (i=0; i < monsterTypes.length; i++) {
+                        //get HD, HD modifier, and display table
+                        var $monsterType = monsterTypes[i];
+
+                        var monsterHD = $monsterType.children(".monsterHD");
+                        var monsterHDMod = $monsterType.children(".monsterHDMod");
+                        var monsterTb = $monsterType.children("monsterToHitTb");
+
+                        var toHitIndex;
+
+                        //use HD to get index in monster table
+                        if (monsterHD == 1) {
+                            if (monsterHDMod < -1) {
+                                   toHitIndex = 0;
+                            } else if (monsterHDMod == -1 {
+                                toHitIndex = 1;
+                            } else if (monsterHDMod == 0 {
+                                toHitIndex = 2;
+                            } else {
+                                toHitIndex = 3;
+                            }
+
+                        } else if (monsterHD >= 16 {
+                            toHitIndex = 11;
+                        } else {
+                            toHitIndex = Math.floor(monsterHD / 2) + 3;
+                        }
+
+
+                        //used HD index to fill table.
+                        var resultsNeeded = monsterTable[toHitIndex];
+                        var resultsDisplay = monsterTb.children("tr:nth-child(2)").children("td");
+
+                        for (i=0; i<resultsDisplay.length); i++) {
+                            resultsDisplay[i + 1].innerHTML(resultsNeeded[i]);
+
+                        }
+
+
+                    }
+                });
+
             });
         };
 
