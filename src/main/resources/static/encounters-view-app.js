674c598a4f7b5415f7bc189e843f86b599c6c5ff
awittig <awittig@gmail.com>
2015-08-05 22:07:45 -0500

    wire up elapsed time inputs

--- src/main/resources/static/encounters-view-app.js
+++ src/main/resources/static/encounters-view-app.js
@@ -12,17 +12,29 @@
         });
 
         this.selectedEncounter = {};
-
         this.encounterDetail = {};
+        this.elapsedTime = {};
 
         this.viewEncounter = function() {
 
             $http.get('/time-tracker/controller/encounter/'+self.selectedEncounter.id).success(function(data){
                 self.encounterDetail = data;
+
+                var seconds = self.encounterDetail.elapsedSeconds;
+
+                var time = secondsToTime(seconds);
+                self.elapsedTime.rounds = time.minutes%10;
+                self.elapsedTime.turns = Math.floor(time.minutes / 10);
+                self.elapsedTime.segments = Math.floor(time.seconds / 6);
+                self.elapsedTime.days = time.days;
+                self.elapsedTime.hours = time.hours;
+                console.log(self.elapsedTime);
             });
         };
 
         this.saveEncounter = function () {
+
+            //TODO reduce self.elapsedTime back into total seconds and update self.encounterDetail
 
             $http.post('/time-tracker/controller/encounter/' + self.selectedEncounter.id, self.encounterDetail).success(function (data) {
                 alert('encounter post: ' + data);
